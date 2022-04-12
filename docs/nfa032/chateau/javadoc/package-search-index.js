bb163d3dbf666020f28bfcfa2a8b4eeeb5f822ca
FarmVivi <farmvividev@gmail.com>
2022-04-12 10:30:33 +0000

    Deploying to main from @ cnam-bfc/NFA032-Chateau@a841b7383efad47429f7bcef970e1e60196dd1ef 🚀

---
+++ docs/nfa032/chateau/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.cnam"},{"l":"net.cnam.entity"},{"l":"net.cnam.object"},{"l":"net.cnam.structure"},{"l":"net.cnam.utils"}];updateSearchResults();
